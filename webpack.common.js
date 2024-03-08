c505cb21506253aaa81bc711950931fd9c0bbba3
Robert P. <porobertdev@gmail.com>
2024-03-08 07:37:03 +0200

    npm: init and initial configuration
    npm install webpack webpack-cli webpack-dev-server style-loader webpack-merge css-loader html-webpack-plugin --save-dev

---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: './src/bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|jpeg|svg|gif)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.ttf$/i,
+                type: 'asset/resource'
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html',
+            title: 'To-Do-List',
+            inject: 'head',
+            scriptLoading: 'defer'
+        })
+    ]
+}
