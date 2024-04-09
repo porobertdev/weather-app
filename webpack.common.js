4861bc3d786aa2c19ecc6c74b929d44f55e23d9e
Robert P. <porobertdev@gmail.com>
2024-04-09 20:32:39 +0300

    npm: install Babel transpiler

--- webpack.common.js
+++ webpack.common.js
@@ -6,30 +6,42 @@ module.exports = {
     output: {
         filename: './src/bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true
+        clean: true,
     },
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
+                use: ['style-loader', 'css-loader'],
             },
             {
                 test: /\.(png|jpg|jpeg|svg|gif)$/i,
-                type: 'asset/resource'
+                type: 'asset/resource',
             },
             {
                 test: /\.ttf$/i,
-                type: 'asset/resource'
-            }
-        ]
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: 'defaults' }],
+                        ],
+                    },
+                },
+            },
+        ],
     },
     plugins: [
         new htmlWebpackPlugin({
             template: './src/index.html',
             title: 'To-Do-List',
             inject: 'head',
-            scriptLoading: 'defer'
-        })
-    ]
-}
+            scriptLoading: 'defer',
+        }),
+    ],
+};
