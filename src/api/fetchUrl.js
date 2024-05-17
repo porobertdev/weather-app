a5354f9b5977fcd3bacced3e5f2d9b8752e8a8be
Robert P. <porobertdev@gmail.com>
2024-05-17 07:39:24 +0300

    api: add `fetch` function

---
+++ src/api/fetchUrl.js
@@ -0,0 +1,15 @@
+import apiKey from './apiKey';
+
+const baseUrl = 'https://api.weatherapi.com/v1/';
+
+export default async function fetchUrl(url) {
+    try {
+        const response = await fetch(`${baseUrl}${url}&key=${apiKey}`);
+
+        if (response.ok) {
+            return response.json();
+        }
+    } catch (err) {
+        console.log('Error occured:', err);
+    }
+}
