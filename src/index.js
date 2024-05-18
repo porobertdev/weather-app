36d71921002937ff3c605d48ed896b0e4367f336
Robert P. <porobertdev@gmail.com>
2024-05-18 18:19:51 +0300

    Revert "database: add DatabaseManager"
    This reverts commit 30398c27f256f0c8dbe1884d4c54982d12386a1b.
    
    * I'll just set the initial location automatically
    * based on IP, so I don't need a database anymore.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
-import DatabaseManager from './database/DatabaseManager';
 import * as weather from './api/weather';
-
-const db = new DatabaseManager('WeatherApp', {
-    data: {
-        location: '',
-    },
-    settings: {
-        api: {
-            // name: {url, apiKey}
-        },
-        unit: 'metric',
-        notifications: false,
-        theme: 'auto',
-    },
-});
-
-db.create();
 
 const data = await weather.getCurrent('Bucharest');
 console.log(data);
 
 // weather.search('Buch');
-
-export default db;
